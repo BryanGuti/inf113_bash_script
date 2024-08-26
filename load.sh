67b9ff1bffa3df2b27a19053eb0c31f258de677b
Bryan Gutierrez Alvarez <bryguti.alvarez@gmail.com>
2024-08-25 20:02:49 -0400

    Create: script load.sh to build the practica.md file

---
+++ load.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+file="practica.md"
+scripts_directory="scripts"
+quantity_of_files=$(($(ls -l $scripts_directory | wc -l)-1))
+
+echo "# Práctica en laboratorio" > $file
+echo "" >> $file
+
+for exercise in `seq $quantity_of_files`
+do
+    echo "***" >> $file
+    echo "" >> $file
+    echo "### *Práctica $exercise*" >> $file
+    echo "" >> $file
+    echo "Archivo: practica$exercise.sh" >> $file
+    echo "" >> $file
+    echo -e "\`\`\`bash" >> $file
+    cat "$scripts_directory/practica$exercise.sh" >> $file
+    echo -e "\`\`\`" >> $file
+    echo "" >> $file
+done
+
