002fc1b6418c1e852c30d313c5829c2970de2553
Bryan Gutierrez Alvarez <bryguti.alvarez@gmail.com>
2024-08-28 17:30:56 -0400

    Update: the presentation's format of load.sh script to build the practica1.md file

--- load.sh
+++ load.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
-file="practica.md"
+file="practica1.md"
 scripts_directory="scripts"
 quantity_of_files=$(($(ls -l $scripts_directory | wc -l)-1))
 
-echo "# Práctica en laboratorio" > $file
+echo "# Práctica 1 Programación Web" > $file
+echo "" >> $file
+echo -e "**Nombre:**  [Bryan Gutierrez Alvarez](https://github.com/BryanGuti/)\n" >> $file
+echo -e "**Paralelo:** B\n" >> $file
+echo -e "**Docente:** Victor Pozo Diaz\n" >> $file
 echo "" >> $file
 
 for exercise in `seq $quantity_of_files`
